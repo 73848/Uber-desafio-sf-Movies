0b632c95453d316c97c58f1019511c74ba4b3f38
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-11-01 12:04:14 -0300

    foram feitas mudancas na requisicao ajax para deixá-la mais reutilizável em outras partes do codigo - vou precisar fazer mais de uma requisicao e afim de manter a performace (sim, eu sei que é apenas um app que talvez ninguem venha a usar, so estou justificando minhas escolhas mesmo)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,86 +1,95 @@
 <!DOCTYPE html>
 <html>
-  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+
 <body>
 
-<br>
-<div id="googleMap" style="width:75%;height:400px;
+    <br>
+    <div id="googleMap" style="width:75%;height:400px;
   margin: 0 auto; 
   padding: 20px;
   background-color: #f0f0f0;">
-  </div>
-<div style=" 
-width:75%;margin: 0 auto; 
-  padding: 20px;">
-  <form action="/geolocation">
-    <label for="">Search</label>
-    <input type="text" id="search" name="search">
-  
-    <button type="submit">Submit</button>
-    <div >
-      <p id="result"></p>
     </div>
-  </form>
-</div>
-<script>
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(37.7749,-122.4194),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-</script>
-<script>
-  $.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
+    <div style=" 
+width:75%;margin: 0 auto; 
+padding: 20px;">
+        <form action="/geolocation">
+            <label for="">Search</label>
+            <input type="text" id="search" name="search">
 
-</script>
-<script>
-  /* AJAX PARA REQUISICOES DA FUNCAO SEARCH */
-$(document).ready(function () {
-  $("#search").on("keyup", function () {
-    /* 
-    $("#result").html(search) ; 
-    */
-   var search = $("#search").val(); 
-   console.log(search);
-    if(search != ''){
-      $.ajax({
-        type: "GET",
-        url: '/search',
-        dataType: "json",
-        data: {'search':search},
-        success: function (data) {
-          var moviesList = $("#movieList");
-          moviesList.empty();
-          var movies = JSON.parse(data.movies);
-          
-          movies.forEach(title => {
-            $('#result').html(title.title); 
-            console.log(title.title);
-            
-          });
-        },
-    error: function(data){
-        console.log(data);
-    }});
-    }else{
-      $("#result").html(" ")
-    }
-    });
-});
-
-$("#result").on('click', function (title) { 
-    var value = $(this).text();
-    $("#search").val(value);
-    $("#search").submit();
-
-})
+            <button type="submit">Submit</button>
+            <div>
+                <p id="result"></p>
+            </div>
+        </form>
+    </div>
+    <script>
+        function myMap() {
+            var mapProp = {
+                center: new google.maps.LatLng(37.7749, -122.4194),
+                zoom: 5,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        }
 
 
-</script>
+        $("#result").on('click', function(title) {
+            var value = $(this).text();
+            $("#search").val(value);
+            $("#search").submit();
 
-<script src="https://maps.googleapis.com/maps/api/js?key={{env("GOOGLE_MAPS_API_KEY")}}&callback=myMap"></script>
+        })
+    </script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'csrftoken': '{{ csrf_token() }}'
+            }
+        });
+    </script>
+    <script>
+        /* AJAX PARA REQUISICOES DA FUNCAO SEARCH */
+        $(document).ready(function() {
+
+
+            function getMovies(search) {
+                if (search != '') {
+                    return $.ajax({
+                        type: "GET",
+                        url: '/search',
+                        dataType: "json",
+                        data: {
+                            'search': search
+                        },
+                    });
+
+                }
+            };
+
+            $("#search").on("keyup", function() {
+                var search = $("#search").val();
+                getMovies(search).then((data) => {
+                    var moviesList = $("#movieList");
+                    moviesList.empty();
+                    var movies = JSON.parse(data.movies);
+                    responseAjax = movies;
+                    movies.forEach(title => {
+                        $('#result').html(title.title);
+                    });
+                }).catch((err) => {
+                    $('#result').html("Refresh a página ou contate o administrador");
+                });
+
+
+
+
+            });
+        });
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=myMap"></script>
 
 </body>
+
 </html>
