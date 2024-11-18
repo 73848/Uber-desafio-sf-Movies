2d60a67369161efa6bc6824e6ea6203d15952f72
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-11-18 14:43:46 -0300

    reescriva da query de search para mysql pleno

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
     public function search(Request $request){
             $input = $request->validate(['search'=>'required|string']);
             $input = strip_tags($input['search']);
-            $result = Movie::query()->where('title','LIKE',  "%{$input}%")->orWhere('locations', 'LIKE', "%{$input}%")->get();
+            $result = DB::select("SELECT * FROM movies WHERE title LIKE '%{$input}%' OR locations LIKE '%{$input}%'");
             $movies = json_encode($result);
             return response()->json(['movies'=>$movies]);
     }
